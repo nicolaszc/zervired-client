9ec8049f1d6895fcb90640e53b38d7b211829669
nicolaszc <nzakrzewicz@gmail.com>
2026-01-27 14:22:50 -0300

    mobile2

--- src/components/providers/FeaturedProviders.tsx
+++ src/components/providers/FeaturedProviders.tsx
@@ -9,10 +9,10 @@ export default function FeaturedProviders() {
   return (
     <section className="mx-auto max-w-7xl px-6 pt-16 pb-8 relative z-10">
       <div className="mb-6 flex items-center justify-between">
-        <h2 className="text-2xl font-semibold bg-radial from-[#efefef] from-40% to-transparent dark:from-[#041f2f] dark:from-40% dark:to-transparent">
+        <h2 className="text-2xl w-full md:w-auto text-center md:text-start font-semibold bg-radial from-[#efefef] from-40% to-transparent dark:from-[#041f2f] dark:from-40% dark:to-transparent">
           Proveedores destacados
         </h2>
-        <span className="text-xs text-amber-600 dark:text-sky-500 uppercase tracking-wide bg-radial from-[#efefef] from-40% to-transparent dark:from-[#041f2f] dark:from-40% dark:to-transparent">
+        <span className="hidden md:inline-block text-xs text-amber-600 dark:text-sky-500 uppercase tracking-wide bg-radial from-[#efefef] from-40% to-transparent dark:from-[#041f2f] dark:from-40% dark:to-transparent">
           Recomendados
         </span>
       </div>
