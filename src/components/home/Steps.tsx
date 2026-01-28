f5a999b9c3c234a3ccdd250b017342544eb61015
nicolaszc <nzakrzewicz@gmail.com>
2026-01-28 02:52:52 -0300

    cat carousel

--- src/components/home/Steps.tsx
+++ src/components/home/Steps.tsx
@@ -15,15 +15,15 @@ export default function Steps({ className = '' }) {
 
             <ol className='list-none p-0 mb-0 mt-8 text-sm flex'>
               <li className='flex flex-col items-center mx-2 w-27 md:mx-6 md:w-42 bg-radial from-[#efefef] from-40% to-transparent dark:from-[#041f2f] dark:from-40% dark:to-transparent'>
-                <FontAwesomeIcon icon={faUserGear} className='text-6xl mb-4' />
+                <FontAwesomeIcon icon={faUserGear} className='text-4xl md:text-6xl mb-4 opacity-50' />
                 <span className='font-extralight dark:font-thin block text-center'>Busca un servicio</span>
               </li>
               <li className='flex flex-col items-center mx-2 w-27 md:mx-6 md:w-42 bg-radial from-[#efefef] from-40% to-transparent dark:from-[#041f2f] dark:from-40% dark:to-transparent'>
-                <FontAwesomeIcon icon={faAward} className='text-6xl mb-4' />
+                <FontAwesomeIcon icon={faAward} className='text-4xl md:text-6xl mb-4 opacity-50' />
                 <span className='font-extralight dark:font-thin block text-center'>Elige un provider verificado</span> 
               </li>
               <li className='flex flex-col items-center mx-2 w-27 md:mx-6 md:w-42 bg-radial from-[#efefef] from-40% to-transparent dark:from-[#041f2f] dark:from-40% dark:to-transparent'>
-                <FontAwesomeIcon icon={faCreditCard} className='text-6xl mb-4' />
+                <FontAwesomeIcon icon={faCreditCard} className='text-4xl md:text-6xl mb-4 opacity-50' />
                 <span className='font-extralight dark:font-thin block text-center'>Agenda y contrata fácil y seguro</span> 
               </li>
             </ol>
--- src/components/providers/ProviderCatgs.tsx
+++ src/components/providers/ProviderCatgs.tsx
@@ -10,31 +10,38 @@ export default function ProviderCatgs() {
         Categorías de Servicio
       </h2>
 
-      <div className="flex justify-around md:justify-between gap-5 md:gap-4 flex-wrap">
-        {Object.entries(CATEGORIES).map(([slug, category]) => {
-          const icon = ICON_MAP[category.icon]
+      <div className="-mx-6 md:mx-0 overflow-x-auto snap-x snap-mandatory scrollbar-hide scroll-px-6">
+        <div className="flex gap-6 px-6 md:grid md:grid-cols-6 md:gap-4 md:px-0">
 
-          return (
-            <Link
-              key={slug}
-              href={`/categorias/${slug}`}
-              className="
-                px-12 py-8 rounded-lg
-                bg-[#f2debe] dark:bg-[#003a57]
-                text-sky-950/90 dark:text-white/90
-                text-xs
-                flex flex-col items-center
-                hover:scale-[1.03] transition
-              "
-            >
-              <FontAwesomeIcon
-                icon={icon}
-                className="text-amber-500/70 dark:text-sky-500/70 text-5xl mb-3"
-              />
-              <p className="text-center">{category.label}</p>
-            </Link>
-          )
-        })}
+          {Object.entries(CATEGORIES).map(([slug, category]) => {
+            const icon = ICON_MAP[category.icon]
+
+            return (
+              <Link
+                key={slug}
+                href={`/categorias/${slug}`}
+                className="
+                  snap-start
+                  min-w-[40%] md:min-w-0
+                  py-8 rounded-lg
+                  bg-[#f2debe] dark:bg-[#003a57]
+                  text-sky-950/90 dark:text-white/90
+                  text-xs
+                  flex flex-col items-center
+                  hover:scale-[1.03] transition
+                "
+              >
+                <FontAwesomeIcon
+                  icon={icon}
+                  className="text-amber-500/70 dark:text-sky-500/70 text-5xl mb-3"
+                />
+                <p className="text-center">{category.label}</p>
+              </Link>
+            )
+          })}
+          {/* Spacer */}
+          <div className="w-px md:hidden shrink-0" />
+        </div>
       </div>
     </div>
   )
