c39d1b77c346802c8a2b6208b43e4dbab846d1da
nicolaszc <nzakrzewicz@gmail.com>
2026-02-12 06:34:52 -0300

    fix: Update condition to render ProvidersSearch component only on mobile

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -40,7 +40,7 @@ export default function Header() {
             </Link>
           </div>
 
-          {!isMobile && ( 
+          {isMobile && ( 
               
               <ProvidersSearch />
             
