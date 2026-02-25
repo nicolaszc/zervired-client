59e6e9fb7f964f9ff1a37dbf7a0dd9283c723478
nicolaszc <nzakrzewicz@gmail.com>
2026-02-24 21:05:14 -0300

    clip

--- src/components/ui/MobileSearch.tsx
+++ src/components/ui/MobileSearch.tsx
@@ -66,7 +66,7 @@ export default function MobileSearch({ className }: Props) {
       id="search"
       onClick={handleBackgroundClick}
       className={cn(
-        "fixed bottom-0 inset-x-0 h-dvh max-h-screen w-full overflow-x-hidden pb-[env(safe-area-inset-bottom)] z-60",
+        "fixed bottom-0 inset-x-0 h-dvh max-h-dvh w-full overflow-x-hidden pb-[env(safe-area-inset-bottom)] z-60",
         "transition-all duration-500 ease-out",
         "bg-linear-to-t gradient",
         className
