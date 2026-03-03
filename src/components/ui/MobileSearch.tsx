d7554254213cf8a030c501572e49e535aafc7853
nicolaszc <nzakrzewicz@gmail.com>
2026-03-03 20:30:11 -0300

    margin bottom

--- src/components/ui/MobileSearch.tsx
+++ src/components/ui/MobileSearch.tsx
@@ -149,7 +149,7 @@ export default function MobileSearch({ className }: Props) {
             aria-label="Activar búsqueda"
           />
         )}
-        <input className="input w-full" placeholder="DEBUG UNCONTROLLED" />
+        <input className="input w-full mb-10" placeholder="DEBUG UNCONTROLLED" />
         <ProvidersSearch ref={searchRef} variant="mobile" className={cn(peek && "pt-4", open && "pt-4")} />
       </div>
       
