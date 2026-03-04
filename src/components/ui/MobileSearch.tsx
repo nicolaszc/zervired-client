8fd8e8849e015528af5d6e74c3a093f104f38ed2
nicolaszc <nzakrzewicz@gmail.com>
2026-03-04 06:06:15 -0300

    160

--- src/components/ui/MobileSearch.tsx
+++ src/components/ui/MobileSearch.tsx
@@ -47,7 +47,7 @@ export default function MobileSearch({ className }: Props) {
   const translateY = useMemo(() => {
     const vh = state.viewport.height
 
-    if (open) return 50
+    if (open) return 160
     if (!peek) return vh
 
     const peekHeight = contentHeight
