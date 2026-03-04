dc1d88067bc42459b0ff182f0a67d6ecdbb43941
nicolaszc <nzakrzewicz@gmail.com>
2026-03-03 22:38:25 -0300

    body x

--- src/hooks/useLockBodyScroll.ts
+++ src/hooks/useLockBodyScroll.ts
@@ -14,13 +14,13 @@ export function useLockBodyScroll(locked: boolean) {
     const prevHtmlStyle = html.getAttribute("style") ?? ""
 
     // Congelar scroll (iOS-safe)
-    /* html.style.height = "100%"
+    html.style.height = "100%"
     body.style.position = "fixed"
     body.style.top = `-${scrollY}px`
     body.style.left = "0"
     body.style.right = "0"
     body.style.width = "100%"
-    body.style.overflow = "hidden" */
+    body.style.overflow = "hidden"
 
     return () => {
       // Restaurar
