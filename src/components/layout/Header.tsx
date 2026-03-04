27d18dba88fdddb04380bb08c2b6a247dd1d981c
nicolaszc <nzakrzewicz@gmail.com>
2026-03-04 07:00:25 -0300

    header

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -19,7 +19,7 @@ export default function Header() {
   const mobileSearchOpen = state.mobileSearchOpen
   return (
     <>
-      {!mobileSearchOpen && (
+
       <header className="bg-(--primary-l) dark:bg-(--secondary-d) fixed z-40 md:z-40 w-full top-0">
         <div className="mx-auto max-w-7xl px-6 py-2.5 flex items-center justify-between bg-(--primary-l) dark:bg-(--secondary-d) relative z-2">
           
@@ -116,7 +116,7 @@ export default function Header() {
           </nav>
         </div>
       </header>
-      )}
+      
    </> 
   )
 }
